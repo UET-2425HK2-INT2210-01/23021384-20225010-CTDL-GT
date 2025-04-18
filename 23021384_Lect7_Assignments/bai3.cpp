7cafa99361e82c7fc8e403c3c5e954b9c55ae9b7
23021384 <23021384@vnu.edu.vn>
2025-04-18 22:55:58 +0700

    Delete 23021384_Lect7_Assignments directory

--- 23021384_Lect7_Assignments/bai3.cpp
+++
@@ -1,7 +0,0 @@
-﻿// hàm tính fibonacci
-int fibonacci(int n)
-{
-	if (n <= 0) return 0; // điều kiện không hợp lệ
-	if (n == 1) return 1; // điều kiện dừng
-	return fibonacci(n - 1) + fibonacci(n - 2); // đệ quy theo công thức
-}
